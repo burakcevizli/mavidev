0d6220867a79dbcd58cfa49450f679437e8514fa
KrieqqeR <burakcevizli@gmail.com>
2024-08-03 23:07:15 +0300

    İlçe Ekle path değişikliliği yapıldı.

--- src/main/java/com/mavidev/mavidev/controller/SehirIlceController.java
+++ src/main/java/com/mavidev/mavidev/controller/SehirIlceController.java
@@ -3,25 +3,23 @@ package com.mavidev.mavidev.controller;
 import com.mavidev.mavidev.entity.SehirIlce;
 import com.mavidev.mavidev.service.SehirIlceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class SehirIlceController {
 
     @Autowired
     private SehirIlceService sehirIlceService;
 
-    @PostMapping("/ekle")
+    @PostMapping("/sehir-ilce-ekle")
     public SehirIlce ekleSehirIlce(@RequestBody SehirIlce sehirIlce){
         return sehirIlceService.ekleSehirIlce(sehirIlce);
     }
 
-    @GetMapping("/getir")
+    @GetMapping("/sehir-listele")
     public List<SehirIlce> getirSehirIlce(){
         return sehirIlceService.getirSehirIlce();
     }
